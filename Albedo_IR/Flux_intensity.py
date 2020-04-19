df02c46326e300b78df4b48cdef4c2daa5642892
leonardocaranti <51086800+leonardocaranti@users.noreply.github.com>
2020-04-19 17:35:25 +0200

    Add files via upload

---
+++ Albedo_IR/Flux_intensity.py
@@ -0,0 +1,69 @@
+import Interpolation
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ------------------------- Derivatives -------------------------------
+
+# Forward, backward euler and central difference derivatives
+def fd_deriv(x, f): return (f[1:] - f[:-1])/(x[1:] - x[:-1]), x[:-1]
+def bd_deriv(x, f): return (f[1:] - f[:-1])/(x[1:] - x[:-1]), x[1:]
+def cd_deriv(x, f): return (f[2:] - f[:-2])/(x[2:] - x[:-2]), x[1:-1]
+
+def plot_derivs():
+    f_fd, x_fd = fd_deriv(xx, spline)
+    f_bd, x_bd = bd_deriv(xx, spline)
+    f_cd, x_cd = cd_deriv(xx, spline)
+
+    plt.suptitle('Element ' + str(element_number) + ' in ' + filename)
+    plt.subplot(211); plt.title("Spline reconstruction"); plt.xlim(t_start, t_end); plt.xlabel("Time [s]"); plt.ylabel("Temperature [°C]")
+    plt.plot(xx, spline, label='Spline reconstruction')
+    plt.subplot(212); plt.title("Numerical derivative");  plt.xlim(t_start, t_end); plt.xlabel("Time [s]"); plt.ylabel(r"$ {\delta T}/{\delta t}\  [K / t] $")
+    plt.plot(x_cd, f_cd, label='Central difference', linestyle='-'); plt.plot(x_bd, f_bd, label='Backward difference derivative', linestyle='-')
+    plt.plot(x_fd, f_fd, label='Forward difference', linestyle='-'); plt.legend(loc='best')
+    plt.axhline(y=0, color = 'k')
+    plt.show()
+
+
+# ------------------------- Incoming heat flux calculations -------------------------------
+
+t_start, t_end, dt = 0, 20000, 0.5                            # Set boundary for graph and spline evaluation
+t_steps = int(t_end/dt) + 1    
+xx = np.linspace(t_start, t_end, t_steps)                     # Set datapoints to evaluate splne at
+
+
+splines = np.zeros(shape = (6, t_steps))
+
+for i in range(2, 8):
+
+    filename, element_number = 'AB2_data/AB2/Mirror_segments.csv', i
+    spline = Interpolation.spline(filename, element_number, xx, t_start, t_end, plotting = False)
+    splines[i-2] = spline
+
+T_front = (splines[0] + splines[2] + splines[4])/3 + 273.15
+T_back = (splines[1] + splines[3] + splines[5])/3 + 273.15
+T_avg = (splines[0] + splines[2] + splines[4] + splines[1] + splines[3] + splines[5])/6 + 273.15
+dT_dt = cd_deriv(xx, T_avg)[0]
+
+T_front, T_back, T_avg, xx = T_front[1:-1], T_back[1:-1], T_avg[1:-1], xx[1:-1]
+sigma = 5.670374*10**(-8)
+e, A, m, C = 0.035, .656*.354*3, 6.5, 690
+
+Q = e * sigma * (T_front*T_front*T_front*T_front + T_back*T_back*T_back*T_back)  +  1/A * m * C * dT_dt
+
+
+# ------------------------- Plotting -------------------------------
+
+plt.suptitle("Primary Mirror Incident Flux Intensity and Average Temperature")
+
+plt.subplot(211)
+plt.title("Incident flux intensity over time")
+plt.plot(xx, Q, color = 'red'); plt.ylabel(r"$ Flux\ intensity\ [W / m^2]  $")
+plt.minorticks_on()
+plt.grid(which='both', axis = 'both', color='#999999', linestyle='-', alpha=0.2)
+
+plt.subplot(212)
+plt.title("Average temperature over time")
+plt.plot(xx, T_avg, color = 'orange');plt.ylabel(r"$ Average\ temperature\ [K] $"); plt.xlabel(r"$ Time\ [s] $")
+plt.minorticks_on()
+plt.grid(which='both', axis = 'both', color='#999999', linestyle='-', alpha=0.2)
+plt.show()
